2c7bf9146edde814dd822a17b9c718e4df19acc5
Somasekhar <sunkarisekhar36@gmail.com>
2025-02-13 20:37:17 +0530

    Updated 13022025

--- pg.mjs
+++ pg.mjs
@@ -1,13 +1,12 @@
 import pkg from "pg"
+import dotenv from "dotenv"
 const { Pool } = pkg;
-
+dotenv.config()
 const pool = new Pool({
-    user: "postgres",
-    host: "database-1.cluster-cvkamekqi3xt.us-east-1.rds.amazonaws.com",
-    database: "dvdrental",
-    password: "sekharamma@176",
-    port: 5432
-
+    user: process.env.PG_USERNAME,
+    host: process.env.PG_URL,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT
 })
-
 export default pool;
